529ea43221fb98561be5e0a7f5db25124709cfbc
Michał Motyczko <mmotyczko@netis.pl>
2015-11-05 10:54:31 +0100

    add missing translation

--- messages/pl/manager.php
+++ messages/pl/manager.php
@@ -51,4 +51,5 @@ return [
   'View user {id}' => 'Szczegóły użytkownika {id}',
   'Yes' => 'Tak',
   'You may optionally enter a comparison operator (<b>&lt;</b>, <b>&lt;=</b>, <b>&gt;</b>, <b>&gt;=</b>, <b>&lt;&gt;</b> or <b>=</b>) at the beginning of each of your search values to specify how the comparison should be done.' => 'Możesz poprzedzić każdą szukaną wartość opcjonalnym operatorem porównania (<b>&lt;</b>, <b>&lt;=</b>, <b>&gt;</b>, <b>&gt;=</b>, <b>&lt;&gt;</b> lub <b>=</b>).',
+  'User account has been successfully created or updated.' => 'Konto użytkownika zostało pomyślnie zapisane.',
 ];
